9ad283f961ea20bf1b110c958ad2fc64eea46697
Yulan Miao <yumiao@adobe.com>
2017-08-18 15:40:26 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # vue-onScrollAnimation
+credits:
+vue-waypoint: https://www.npmjs.com/package/vue-waypoint
+
+Experimenting using vue-waypoint to animate on scroll
+demo: https://cl.ly/18080b3x0m1u
+
+using vue-webpack-boilerplate
+
+## Usage
+
+``` bash
+$ npm install -g vue-cli
+$ vue init webpack my-project
+$ cd my-project
+$ npm install
+$ npm run dev
---
+++ my-project/src/components/Hello.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="hello container">
+    <div class="content" v-bind:class="{active: isSignUpActive}">
+      <div class="statement">
+        <h1 class="title">More than 10 million creatives</h1>
+
+        <a class="signupLink" v-bind:class="{active: isSignUpActive}" href="/signup">Sign Up.</a>
+      </div>
+    </div>
+
+    <div class="trigger">
+        <v-waypoint @waypoint="signUpHandler"></v-waypoint>
+    </div>
+
+    <div class="trigger">
+        <v-waypoint @waypoint="inHandler"></v-waypoint>
+    </div>
+
+    <div class="jobs staticHide " v-bind:class="{active: isActive}">
+      <div class="jobBody">
+        Interested in joining our team? See our <a class="link" href="/careers">open jobs.</a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      isActive: false,
+      isSignUpActive: false
+    }
+  },
+  methods: {
+    inHandler () {
+      this.isActive = true
+    },
+    signUpHandler () {
+      this.isSignUpActive = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.container {
+  height: calc(100vh - 60px + 120px);
+  position: relative;
+}
+
+.content {
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+}
+
+.content.active {
+  background-image: url("../assets/signUpBackground.png");
+}
+
+.statement {
+  text-align: center;
+}
+
+.title {
+  color: white;
+  font-size: 80px;
+  font-weight: bold;
+  line-height: 1;
+}
+
+.signupLink {
+  color: white;
+  display: none;
+  font-size: 80px;
+  font-weight: bold;
+  line-height: 1;
+  text-decoration: underline;
+}
+
+.signupLink.active {
+  display:  block;
+}
+
+.jobs {
+  align-items: center;
+  background-color: white;
+  display: flex;
+  font-size: 25px;
+  font-weight: bold;
+  height: 120px;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+}
+
+.trigger {
+  bottom:  0;
+  width:  100%;
+}
+
+.staticHide {
+  display:  none;
+}
+.staticHide.active {
+  display: flex;
+}
+
+.link {
+  color: black;
+  font-weight: bold;
+  text-decoration: underline;
+}
+</style>
